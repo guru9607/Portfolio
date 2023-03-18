70931978ac0aa4f5a4b96fedec7077b00114d73a
Guruprasad <guruprasadgaikwad8@gmail.com>
2023-03-18 10:50:22 +0530

    Done will Skills section

---
+++ components/Skillcard.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Fade } from 'react-awesome-reveal';
+
+
+export function SkillCard(props: any) {
+  
+  const { title, tags, key, image, icon } = props;
+
+  return (
+<div className="flex flex-wrap place-items-center h-screen/2 lg:mr-6 md:mb-5">
+    <div className="overflow-hidden shadow-lg transition duration-500 ease-in-out hover:shadow-2xl h-auto w-lg md:w-80 m-auto hover:border-gradient-br-orange-blue-50 border-gradient-br-blue-orange-50 dark:border-gradient-br-blue-orange-bl dark:hover:border-gradient-br-orange-blue-bl border-transparent border-solid border-4 rounded-xl -mb-5 mt-8 ">
+    <div className="w-full p-4 -mt-2">
+    <p className="text-black dark:text-white text-xl font-regular font-Dosis -mb-6">
+    {icon}
+        </p>
+                <p className="text-orange text-xl font-regular font-Dosis mb-3 pl-8">
+                {title}
+            </p>
+                <div className="flex flex-wrap justify-starts items-center text-xs text-white font-light font-Dosis -m-1">
+                {tags.map((tag : string, i: number) => (
+                  <Fade direction="up" triggerOnce key={i} delay={500}>
+                  <div className="ml-1 mr-1 mb-1 px-2 py-1 rounded bg-blue-600" key={tag.toString()}>
+                    {tags[i]}
+                    </div>
+                  </Fade>
+                ))}
+                </div>
+            </div>
+    </div>
+</div>
+  )
+}
---
+++ components/Skills.tsx
@@ -0,0 +1,73 @@
+import {SkillCard} from "./Skillcard"
+import web from "../images/web.png"
+import { SiTorbrowser, SiExpress, SiNextdotjs, SiOctopusdeploy } from "react-icons/si"
+import { AiFillCode } from "react-icons/ai"
+import { MdOutlineDesignServices } from "react-icons/md"
+import { Fade } from "react-awesome-reveal"
+
+
+const Skills = () => {
+
+    const skills = [
+        {
+            id:1,
+            title: "Web Development",
+            tags: ['HTML5', 'CSS3', 'Sass'],
+            image: web,
+            icon: <SiTorbrowser />
+        },
+        {
+            id:2,
+            title: "Languages",
+            tags: ['C', 'C++', 'JavaScript', 'Python', 'TypeScript'],
+            image: web,
+            icon: <AiFillCode />
+        },
+        {
+            id:3,
+            title: "Backend Frameworks",
+            tags: ['Express'],
+            image: web,
+            icon: <SiExpress />
+        },
+        {
+            id:4,
+            title: "Frontend Frameworks",
+            tags: ['NextJs', 'ReactJs'],
+            image: web,
+            icon: <SiNextdotjs/>
+        },
+        {
+            id:5,
+            title: "Deployment",
+            tags: ['Netlify', 'Vercel'],
+            image: web,
+            icon: <SiOctopusdeploy />
+        },
+        {   
+            id:6,
+            title: "Design",
+            tags: ['Figma', 'Canva', 'Photoshop'],
+            image: web,
+            icon: <MdOutlineDesignServices />
+        },
+    ]
+    return (
+        <div className="container px-8 py-5 mx-auto max-w-5xl lg:mb-10">
+        <Fade direction="down" triggerOnce = {true}>
+         <h1 className="text-center font-Dosis text-3xl font-semibold text-gray-900 lg:text-5xl md:text-5xl dark:text-white md:mb-14 lg:mb-24 "><a className="text-orange">Skills</a> <a className="text-blue-600">(⌐■_■)</a></h1>
+        </Fade>
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 lg:-mt-20 ">
+
+            {skills.map((item) => (
+                <Fade direction="down" triggerOnce = {true} delay={100*item.id} key={item.id}>
+                <SkillCard title={item.title} key={item.id} tags={item.tags} image={item.image} icon={item.icon}/>
+                </Fade>
+              ))}
+            </div>
+            <hr className="bg-gray-400 mt-16 dark:bg-gray-800"/>
+        </div>
+    )
+}
+
+export default Skills;
